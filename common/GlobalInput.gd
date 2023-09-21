79c7d3430e03e1648b00649ca698a09c599893d3
gustavo domingues <deathdiec@gmail.com>
2023-09-21 12:51:06 -0300

    feat: weapon rotate + global input

--- common/GlobalInput.gd
+++ common/GlobalInput.gd
@@ -1,22 +1,19 @@
-extends Sprite
+extends Node
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-export var damage : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Function that will set the damage of the weapon based in the character\
-func set_damage(damage):
-	self.damage = damage
-	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	look_at(get_global_mouse_position())
+func _process(_delta):
+	if Input.is_action_pressed("exit_game"):
+		get_tree().quit()
+		pass
 	pass
