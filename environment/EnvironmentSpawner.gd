8009438cafcf8d87898aec1c508f46108abce228
gustavo domingues <deathdiec@gmail.com>
2023-10-03 12:09:11 -0300

    feat: rocks + enviroment spawner fully finished

--- environment/EnvironmentSpawner.gd
+++ environment/EnvironmentSpawner.gd
@@ -10,26 +10,32 @@ var iteration = 0
 # Variables
 export var plant_offset_x = 50
 export var plant_offset_y = 50
+export var rock_offset_x = 100
+export var rock_offset_y = 100
 export var plant_amount = 100
+export var rock_amount = 200
 
 # Resources
 onready var plant = preload("res://Environment//Plant.tscn")
+onready var rock = preload("res://Environment//Rock.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Call the function to spawn the plants
 	spawn_plants()
+	iteration = 0
+	spawn_rocks()
 	pass # Replace with function body.
 
 # Function that will instantiate the plants
 func spawn_plants() -> void:
-	iteration += 1
-	if iteration > plant_amount:
-		return
 	# Get the random position
 	var random_position = Vector2(rand_range(-Global.BORDER_X, Global.BORDER_X - plant_offset_x), rand_range(-Global.BORDER_Y, Global.BORDER_Y - plant_offset_y))
 	# Check if the position is already used
 	if used_positions.find(random_position) == -1:
+		iteration += 1
+		if iteration > plant_amount:
+			return
 		# Add the position to the used positions array
 		used_positions.append(random_position)
 		
@@ -42,8 +48,30 @@ func spawn_plants() -> void:
 		spawn_plants()
 	else:
 		# Call the function again
-		print_debug("calling")
 		spawn_plants()
+	
+# Function that will instantiate the rocks
+func spawn_rocks() -> void:
+	# Get the random position
+	var random_position = Vector2(rand_range(-Global.BORDER_X, Global.BORDER_X - rock_offset_x), rand_range(-Global.BORDER_Y, Global.BORDER_Y - rock_offset_y))
+	# Check if the position is already used
+	if used_positions.find(random_position) == -1:
+		iteration += 1
+		if iteration > rock_amount:
+			return
+		# Add the position to the used positions array
+		used_positions.append(random_position)
+		
+		# Instantiate the rock
+		var rock_instance = rock.instance()
+		rock_instance.position = random_position
+		add_child(rock_instance)
+		
+		# Call the function again
+		spawn_rocks()
+	else:
+		# Call the function again
+		spawn_rocks()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- environment/EnvironmentSpriteController.gd
+++ environment/EnvironmentSpriteController.gd
@@ -5,10 +5,15 @@ extends Node
 # var a = 2
 # var b = "text"
 
+# Export var that will contain an array of textures
+export var textures := []
+export var has_animated_sprite := true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite_random_animated()
+	if has_animated_sprite : sprite_random_animated()
+	else : sprite_random()
 	pass # Replace with function body.
 
 # Function that will get the parent animatedsprite child and change the animation from "idle" or "idle1" randomly
@@ -24,6 +29,12 @@ func sprite_random_animated() -> void:
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	animatedsprite.set_speed_scale(rng.randf_range(0.35, 1.5))
+
+# Function that will change the sprite randomly from the array of textures
+func sprite_random() -> void:
+	var sprite = get_parent().get_node("Sprite")
+	var random = randi()%textures.size()
+	sprite.set_texture(textures[random])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
